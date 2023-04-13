aac363b8f4eb4fb94464321291440410b2126fc6
Pablo <pablogcg05@gmail.com>
2023-04-13 11:14:04 +0200

    Intento 1

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
 Console.WriteLine("Hola, el siguiente programa le pedirá dos números para sumarlos, restarlos, multiplicarlos y dividirlos");
 Console.WriteLine("Introduzca el primer número"); 
 var primerNumero = Console.ReadLine();
+int numero1 = int.Parse(primerNumero);
 Console.WriteLine("Introduzca el segudno número"); 
 var segundoNumero = Console.ReadLine();
-Console.WriteLine("El resultado de sumar los números " + primerNumero + "y" + segundoNumero + " es: " + Sumar() );
+int numero2 = int.Parse(segundoNumero); 
+var suma = numero1 + numero2;
+var resta = numero1 - numero2;
+var producto = numero1 * numero2;
+var division = numero1 / numero2;
+Console.WriteLine("El resultado de sumar los números " + primerNumero + "y" + segundoNumero + " es: ");
