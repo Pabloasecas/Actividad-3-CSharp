ad427078ac0953b302fb601dd01d587769abf859
Pablo <pablogcg05@gmail.com>
2023-04-13 11:00:25 +0200

    Pruebas

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hola, el siguiente programa le pedirá dos números para sumarlos, restarlos, multiplicarlos y dividirlos");
+Console.WriteLine("Introduzca el primer número"); 
+var primerNumero = Console.ReadLine();
+Console.WriteLine("Introduzca el segudno número"); 
+var segundoNumero = Console.ReadLine();
+Console.WriteLine("El resultado de sumar los números " + primerNumero + "y" + segundoNumero + " es: " + Sumar() );
